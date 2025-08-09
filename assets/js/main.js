9d53aba67f99b396f77c419f76947336e22723dc
Bururich <rgubrij@gmail.com>
2025-08-09 23:29:49 +0200

    amenities, how to buy and about us sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// hero video function//
+
 document.addEventListener('DOMContentLoaded', () => {
     const video = document.getElementById('myVideo');
     const playBtn = document.getElementById('playBtn');
@@ -28,3 +30,103 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+//End hero video function//
+//Project image change function//
+
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.getElementById("project-image");
+
+    const projects = {
+        "project-first": {
+            src: "/assets/img/projects-europe.PNG",
+            alt: "Проект Сузір'я Європейське"
+        },
+        "project-second": {
+            src: "/assets/img/project-sky.jpg",
+            alt: "Проект Сузір'я Скай"
+        },
+        "project-third": {
+            src: "/assets/img/projects-sythiria2020.jpg",
+            alt: "Проект Сузір'я 2020"
+        },
+        "project-fourth": {
+            src: "/assets/img/projects-sythiria2019.jpg",
+            alt: "Проект Сузір'я 2019"
+        }
+    };
+
+    document.querySelectorAll(".project-item a").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const projectData = projects[this.id];
+            if (projectData) {
+                image.src = projectData.src;
+                image.alt = projectData.alt;
+            }
+        });
+    });
+});
+
+//End Project image change//
+//Project list active class function//
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectLinks = document.querySelectorAll(".project-list a");
+
+    projectLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            // Убираем класс у всех
+            projectLinks.forEach(l => l.classList.remove("active-project"));
+            // Добавляем класс к нажатой
+            this.classList.add("active-project");
+        });
+    });
+});
+
+//End Project list active class function//
+//Amenities image change function//
+
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.getElementById("amenities-image");
+
+    const projects = {
+        "amenities-first": {
+            src: "/assets/img/amenties-.PNG",
+            alt: "ремонт під ключ"
+        },
+        "amenities-second": {
+            src: "/assets/img/amenities-1.PNG",
+            alt: "Проект дизайн"
+        }
+    };
+
+    document.querySelectorAll(".amenities-item a").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const projectData = projects[this.id];
+            if (projectData) {
+                image.src = projectData.src;
+                image.alt = projectData.alt;
+            }
+        });
+    });
+});
+
+//End Amenities image change//
+//Amenities list active class function//
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectLinks = document.querySelectorAll(".amenities-list a");
+
+    projectLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            // Убираем класс у всех
+            projectLinks.forEach(l => l.classList.remove("active-project"));
+            // Добавляем класс к нажатой
+            this.classList.add("active-project");
+        });
+    });
+});
+
+//End Amenities list active class function//
